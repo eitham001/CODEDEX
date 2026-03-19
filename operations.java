b9a75b9542560d9a4c9aa6e1c1c8d52c0b4ac63c
Eitham Bowen Robins <eitham.bowenrob@ug.edu.ec>
2026-03-19 16:54:43 -0500

    Create operations.java

---
+++ operations.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        double chemicalA = 20.5;
+        double chemicalB = 6.20;
+
+        double result = (chemicalA + chemicalB) / (chemicalA * chemicalB);
+
+        System.out.println(result);
+    }
+}
