16bda9158d00f4cfa1ec023464445ed4560d692d
Eitham Bowen Robins <eitham.bowenrob@ug.edu.ec>
2026-03-19 16:39:18 -0500

    Create datos.java

---
+++ datos.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "eitham001";
+        String state = "California";
+        char gender = 'M';
+        int age = 20;
+        boolean organDonor = false;
+
+        System.out.println("Name: " + name);
+        System.out.println("State: " + state);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Donor: " + organDonor);
+    }
+}
