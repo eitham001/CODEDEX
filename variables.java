bd2752fca91a59b51b92d6ebbdb4d5bba8149aa6
Eitham Bowen Robins <eitham.bowenrob@ug.edu.ec>
2026-03-19 16:29:39 -0500

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int LivingRoom = 15;
+        int Bedroom = 45;
+        int Bathroom = 10;
+
+        System.out.println(LivingRoom);
+        System.out.println(Bedroom);
+        System.out.println(Bathroom);        
+
+        int total = LivingRoom + Bedroom + Bathroom;
+        System.out.println("\nLa suma total es: " + total);
+    }
+}
